6d461f60a192ce7eb93f55e2cbae491af4c44f17
JunixOs <yonelsilva75@gmail.com>
2025-01-04 23:45:51 -0500

    Cambio de la pagina principal, separacacion de los temas de la pagina web por secciones. Nuevos archivos añadidos.

--- estructura.sql
+++ estructura.sql
@@ -1,20 +1,16 @@
 
-
 CREATE TABLE IF NOT EXISTS Users
 ( 
-    ID int(10) NOT NULL,
+    ID INT PRIMARY KEY NOT NULL,
     Nombre CHARACTER(25) NULL,
     Apellido CHARACTER(25) NOT NULL,
-    Telefono int(11) NOT NULL,
-    Email varchar(255) NULL,
-    PRIMARY KEY (ID)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+    Telefono INT NOT NULL,
+    Email varchar(255) NULL
+)
 
 INSERT INTO Users (id,Nombre,Apellido,Telefono,Email)
 VALUES
     (1,"Yonel Junior","Ordoñez Silva",934564789,"yonel7@hotmail.com"),
     (2,"Amelia","Mauricio Solorzano",945342345,"mau69@yahho.com");
-
-
 
 
